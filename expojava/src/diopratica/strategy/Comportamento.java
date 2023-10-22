1b68462e982867de2b650286ba9863aa69b669ce
Zero <68662867+snagzero@users.noreply.github.com>
2023-10-22 18:25:52 -0300

    strategy type
    update

---
+++ expojava/src/diopratica/strategy/Comportamento.java
@@ -0,0 +1,5 @@
+package diopratica.strategy;
+
+public interface Comportamento {
+	void mover();
+}
---
+++ expojava/src/diopratica/strategy/ComportamentoNormal.java
@@ -0,0 +1,10 @@
+package diopratica.strategy;
+
+public class ComportamentoNormal implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se normalmente...");
+	}
+
+}
